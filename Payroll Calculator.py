31ca33732881e269ff41790367e2f4551354f0fd
henleyhva <henley.hva@gmail.com>
2026-08-21 15:56:21 +0800

    Update Payroll Calculator.py

--- Payroll Calculator.py
+++ Payroll Calculator.py
@@ -1,47 +1,74 @@
-name = input("Employee name: ")
-rate_hr = float(input("Hourly rate: ").replace(",", ""))
-work_hr = float(input("Regular Hours Worked: ").replace(",", ""))
-ot_hr = float(input("Overtime Hours: ").replace(",", ""))
-bonus = float(input("Bonus: ").replace(",", ""))
-regular_pay = float(rate_hr * work_hr)
-ot_pay = float(ot_hr * 1.5 * rate_hr)
-gross_pay = float(regular_pay + ot_pay + bonus)
+name = input("Name: ")
+
+def valid_num (prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value < 0:
+                print("Please enter a positive number")
+                continue
+            return value
 
 
-if gross_pay <= 20000:
-    tax = gross_pay * 0.05
+        except ValueError:
+            print("Please enter a numeric value")
 
-elif gross_pay > 20000 and gross_pay <= 40000:
-    tax = gross_pay * 0.1
+rate_hr = valid_num("Hourly rate: ")
+work_hr = valid_num("Working hours: ")
+ot_hr = valid_num("Overtime hours: ")
+bonus = valid_num("Bonus: ")
+OT_MULTIPLIER = 1.5
 
-elif gross_pay > 40000:
-    tax = gross_pay * 0.15
+def calc_RP (work_hr, rate_hr):
+    regular_pay = rate_hr * work_hr
+    return regular_pay
 
-net_pay = (gross_pay - tax)
+def calc_op (ot_hr, OT_MULTIPLIER, rate_hr):
+    ot_pay = ot_hr * OT_MULTIPLIER * rate_hr
+    return ot_pay
+
+def calc_gp (regular_pay, ot_pay, bonus):
+    gross_pay = regular_pay + ot_pay + bonus
+    return gross_pay
+
+regular_pay = calc_RP (work_hr, rate_hr)
+ot_pay = calc_op (ot_hr, OT_MULTIPLIER, rate_hr)
+gross_pay = calc_gp (regular_pay, ot_pay, bonus)
+
+
+def calc_tax (gross_pay):
+    if gross_pay <= 20000:
+        return gross_pay * 0.05
+
+    elif 20000 < gross_pay <= 40000:
+        return gross_pay * 0.10
+
+    else:
+        return gross_pay * 0.15
 
 
 
-print("=============================")
-print("       PAYROLL SUMMARY       ")
-print("=============================")
-print("")
+tax = calc_tax (gross_pay)
+
+def calc_np (gross_pay, tax):
+    net_pay = gross_pay - tax
+    return net_pay
+
+net_pay = calc_np (gross_pay, tax)
+
+print("===============================")
+print("         Payroll Summary       ")
+print("===============================")
+print()
 print("Employee: " + name)
-print("")
-print(f"Hourly rate: ₱{rate_hr:,}")
-print(f"Regular Hours: {work_hr:,}")
-print(f"Overtime Hours: {ot_hr:,}")
-print(f"Bonus: ₱{bonus:,}")
-print('')
-print(f"Regular pay: ₱{regular_pay:,}")
-print(f"Overtime pay: ₱{ot_pay:,}")
-print(f"Gross pay: ₱{gross_pay:,}")
-print('')
-print(f"Tax: ₱{tax:,}")
-print(f"Net pay: ₱{net_pay:,}")
-
-
-
-
-
-
-
+print()
+print(f"Hourly rate: ₱{rate_hr:,.2f}")
+print(f"Regular hours:  {work_hr:,.2f}")
+print(f"Overtime hours:  {ot_hr:,.2f}")
+print(f"Bonus: ₱{bonus:,.2f}")
+print("-------------------------------")
+print(f"Regular pay: ₱{regular_pay:,.2f}")
+print(f"Overtime pay: ₱{ot_pay:,.2f}")
+print(f"Gross pay: ₱{gross_pay:,.2f}")
+print(f"Tax: ₱{tax:,.2f}")
+print(f"Net pay: ₱{net_pay:,.2f}")
